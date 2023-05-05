63767a5d2d0d839d0a3a18bd74b1ad147cf4801f
mirkbirki <mirlindbacaj@gmail.com>
2023-05-05 21:16:23 +0200

    i dont know

--- main.py
+++ main.py
@@ -1,69 +1,74 @@
 # beräkna lön inkl. alla tillägg. Importera timmar från google kalender. Importera ob-tillägg
 # från nätet. Använd jobbskuld för att räkna ut skatten.
 
+# Salary before any additional salary supplements.
 basic_salary = 28000
+# Salary for any extra hour worked.
 salary_extra_hour = 190
+# Amount of extra hours worked during the month.
 extra_hours = int(input("Amount of extra hours: "))
-ob = 4500
-gross_salary = grundlon + ob + lon_extratimme * extratimmar
+# Salary supplements for hours worked during nights, weekends and holidays (inconvenient working hours).
+salary_supplements = 4500
+# Salary before taxes
+gross_salary = basic_salary + salary_extra_hour * extra_hours + salary_supplements
 
 print("Gross salary:", gross_salary)
 
 # Skattetabeller
 
-if 30001 <= bruttolon <= 30200:
-    skatt = 5943
-elif 30201 <= bruttolon <= 30400:
-    skatt = 5994
-elif 30401 <= bruttolon <= 30600:
-    skatt = 6045
-elif 30601 <= bruttolon <= 30800:
-    skatt = 6096
-elif 30801 <= bruttolon <= 31000:
-    skatt = 6147
-elif 31001 <= bruttolon <= 31200:
-    skatt = 6198
-elif 31201 <= bruttolon <= 31400:
-    skatt = 6250
-elif 31401 <= bruttolon <= 31600:
-    skatt = 6301
-elif 31601 <= bruttolon <= 31800:
-    skatt = 6352
-elif 31801 <= bruttolon <= 32000:
-    skatt = 6403
-elif 32200 >= bruttolon >= 32001:
-    skatt = 6454
-elif 32400 >= bruttolon >= 32201:
-    skatt = 6505
-elif 32600 >= bruttolon >= 32401:
-    skatt = 6556
-elif 32800 >= bruttolon >= 32601:
-    skatt = 6607
-elif 33000 >= bruttolon >= 32801:
-    skatt = 6658
-elif 33400 >= bruttolon >= 33201:
-    skatt = 6761
-elif 33600 >= bruttolon >= 33401:
-    skatt = 6812
-elif 33800 >= bruttolon >= 33601:
-    skatt = 6863
-elif 34000 >= bruttolon >= 33801:
-    skatt = 6914
-elif 34200 >= bruttolon >= 34001:
-    skatt = 6965
-elif 34400 >= bruttolon >= 34201:
-    skatt = 7016
-elif 34600 >= bruttolon >= 34401:
-    skatt = 7067
-elif 34800 >= bruttolon >= 34601:
-    skatt = 7118
-elif 35000 >= bruttolon >= 34601:
-    skatt = 7169
+if 30001 <= gross_salary <= 30200:
+    taxes = 5943
+elif 30201 <= gross_salary <= 30400:
+    taxes = 5994
+elif 30401 <= gross_salary <= 30600:
+    taxes = 6045
+elif 30601 <= gross_salary <= 30800:
+    taxes = 6096
+elif 30801 <= gross_salary <= 31000:
+    taxes = 6147
+elif 31001 <= gross_salary <= 31200:
+    taxes = 6198
+elif 31201 <= gross_salary <= 31400:
+    taxes = 6250
+elif 31401 <= gross_salary <= 31600:
+    taxes = 6301
+elif 31601 <= gross_salary <= 31800:
+    taxes = 6352
+elif 31801 <= gross_salary <= 32000:
+    taxes = 6403
+elif 32200 >= gross_salary >= 32001:
+    taxes = 6454
+elif 32400 >= gross_salary >= 32201:
+    taxes = 6505
+elif 32600 >= gross_salary >= 32401:
+    taxes = 6556
+elif 32800 >= gross_salary >= 32601:
+    taxes = 6607
+elif 33000 >= gross_salary >= 32801:
+    taxes = 6658
+elif 33400 >= gross_salary >= 33201:
+    taxes = 6761
+elif 33600 >= gross_salary >= 33401:
+    taxes = 6812
+elif 33800 >= gross_salary >= 33601:
+    taxes = 6863
+elif 34000 >= gross_salary >= 33801:
+    taxes = 6914
+elif 34200 >= gross_salary >= 34001:
+    taxes = 6965
+elif 34400 >= gross_salary >= 34201:
+    taxes = 7016
+elif 34600 >= gross_salary >= 34401:
+    taxes = 7067
+elif 34800 >= gross_salary >= 34601:
+    taxes = 7118
+elif 35000 >= gross_salary >= 34601:
+    taxes = 7169
 else:
-    skatt = bruttolon * 0.3
+    taxes = gross_salary * 0.3
 
-print("Taxes:", skatt)
+print("Taxes:", taxes)
 
-nettolon = bruttolon - skatt
+net_salary = gross_salary - taxes
 
-print("Net salary:", nettolon)
+print("Net salary:", net_salary)
