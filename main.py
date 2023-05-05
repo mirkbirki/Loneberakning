2e645d96740adbc2b7b69d85774d05f890965747
mirkbirki <mirlindbacaj@gmail.com>
2023-05-05 10:59:00 +0200

    first commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 # beräkna lön inkl. alla tillägg. Importera timmar från google kalender. Importera ob-tillägg
 # från nätet. Använd jobbskuld för att räkna ut skatten.
 
+grundlon = int(input("Grundlön? "))
+lon_extratimme = 190
