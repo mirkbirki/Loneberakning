de909b63cb3864bc627851f95624d5e6ac054fc5
mirkbirki <mirlindbacaj@gmail.com>
2023-05-05 21:00:13 +0200

    i dont know

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 # beräkna lön inkl. alla tillägg. Importera timmar från google kalender. Importera ob-tillägg
 # från nätet. Använd jobbskuld för att räkna ut skatten.
 
-grundlon = 28000
-lon_extratimme = 190
-extratimmar = int(input("Antal extratimmar: "))
+basic_salary = 28000
+salary_extra_hour = 190
+extra_hours = int(input("Amount of extra hours: "))
 ob = 4500
-bruttolon = grundlon + ob + lon_extratimme * extratimmar
+gross_salary = grundlon + ob + lon_extratimme * extratimmar
 
-print("Bruttolon:", bruttolon)
+print("Gross salary:", gross_salary)
 
 # Skattetabeller
 
-if 32200 >= bruttolon >= 32001:
+if 30001 <= bruttolon <= 30200:
+    skatt = 5943
+elif 30201 <= bruttolon <= 30400:
+    skatt = 5994
+elif 30401 <= bruttolon <= 30600:
+    skatt = 6045
+elif 30601 <= bruttolon <= 30800:
+    skatt = 6096
+elif 30801 <= bruttolon <= 31000:
+    skatt = 6147
+elif 31001 <= bruttolon <= 31200:
+    skatt = 6198
+elif 31201 <= bruttolon <= 31400:
+    skatt = 6250
+elif 31401 <= bruttolon <= 31600:
+    skatt = 6301
+elif 31601 <= bruttolon <= 31800:
+    skatt = 6352
+elif 31801 <= bruttolon <= 32000:
+    skatt = 6403
+elif 32200 >= bruttolon >= 32001:
     skatt = 6454
 elif 32400 >= bruttolon >= 32201:
     skatt = 6505
@@ -42,8 +62,8 @@ elif 35000 >= bruttolon >= 34601:
 else:
     skatt = bruttolon * 0.3
 
-print("Skatt:", skatt)
+print("Taxes:", skatt)
 
 nettolon = bruttolon - skatt
 
-print("Nettolon:", nettolon)
+print("Net salary:", nettolon)
